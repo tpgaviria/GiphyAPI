e7d9ce6a78d45ca018a423efa9e9ea1d5ba49038
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-04-01 19:16:18 -0400

    full script working

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,10 +2,45 @@
 var queries = ["love", "kiss", "i'm hungry", "hurry", "please", "i miss you"];
 
 
-// var gifDiv;
-// var rating;
-// var stillImg;
-// var movingImg;
+
+
+function createButtons() {
+
+    $('.button-container').empty();
+
+    for (var i = 0; i < queries.length; i++) {
+        var queryButton = $('<button>');
+        queryButton.addClass('query-button')
+            .attr('term', queries[i])
+            .text(queries[i]);
+        $('.button-container').append(queryButton);
+    }
+
+    $(document).on('click', '.query-button', displayGifs);
+};
+
+
+
+// when input submit button clicked, input text is added to queries array
+$('#add-term').on('click', function (event) {
+
+    event.preventDefault();
+
+    // input saved as variable
+    var newTerm = $('#new-search').val().trim();
+
+    // input pushed into queries array
+    queries.push(newTerm);
+
+    //
+    createButtons();
+
+})
+
+
+var stillImg;
+var apiPromise;
+
 // function using giphy's api
 // displays gifs using buttons 'term' attribute to generate appropriate query url
 function displayGifs() {
@@ -28,16 +63,14 @@ function displayGifs() {
 
         var gifDiv;
         var rating;
-        var stillImg;
-        var movingImg;
+        apiPromise = response;
 
         console.log(searchTerm);
         console.log(queryURL);
         console.log(response);
         console.log(response.data.length);
 
-                for (var i = 0; i < response.data.length; i++) {
-
+        for (var i = 0; i < response.data.length; i++) {
             gifDiv = $('<div class="gif-box">');
             rating = response.data[i].rating;
             stillImg = response.data[i].images.original_still.url;
@@ -53,64 +86,32 @@ function displayGifs() {
                 .append(rating);
 
             $('.gif-results').append(gifDiv);
-
-
-
-
-            $(document).on('click', '.gif', function (event) {
-
-                movingImg = response.data[$(this).attr('index')].images.original.url;
-                stillImg = response.data[$(this).attr('index')].images.original_still.url;
-
-                var movement = $(this).attr('movement');
-
-                // if movement attr is still, onclick change to animate and show moving gif
-                if (movement === 'still') {
-                    $(this).attr('movement', 'animate');
-                    $(this).attr('src', movingImg);
-                } else {
-                    //if movement attr is still, onclick change to still and show still image
-                    $(this).attr('movement', 'still');
-                    $(this).attr('src', stillImg);
-                }
-            });
-
-
         }
+
     })
 }
 
 
+$(document).on('click', 'img.gif', function (event) {
 
+    var movingImg;
 
-function createButtons() {
-    $('.button-container').empty();
-    for (var i = 0; i < queries.length; i++) {
-        var queryButton = $('<button>');
-        queryButton.addClass('query-button')
-            .attr('term', queries[i])
-            .text(queries[i]);
-        $('.button-container').append(queryButton);
+    movingImg = apiPromise.data[$(this).attr('index')].images.original.url;
+    stillImg = apiPromise.data[$(this).attr('index')].images.original_still.url;
+
+    var movement = $(this).attr('movement');
+
+    // if movement attr is still, onclick change to animate and show moving gif
+    if (movement === 'still') {
+        $(this).attr('movement', 'animate');
+        $(this).attr('src', movingImg);
+    } else {
+        //if movement attr is still, onclick change to still and show still image
+        $(this).attr('movement', 'still');
+        $(this).attr('src', stillImg);
     }
+});
 
-    console.log(document);
-    $(document).on('click', '.query-button', displayGifs);
-    
-}
-
-// when input submit button clicked, input text is added to queries array
-$('#add-term').on('click', function (event) {
-    event.preventDefault();
-
-    // input saved as variable
-    var newTerm = $('#new-search').val().trim();
-
-    // input pushed into queries array
-    queries.push(newTerm);
-
-    //
-    createButtons();
-})
 
 
 createButtons();
